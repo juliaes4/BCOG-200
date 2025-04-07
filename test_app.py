5a8e1670e9e28df05bf32932ebcdaa508501c15c
juliaes4 <juliaes4@illinois.edu>
2025-04-06 21:45:28 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,1 +1,19 @@
 
+"""
+TEST PLAN
+
+i want to ensure the Healthy Breakfast Finder works correctly when searching for different ingredients or diet tags.
+
+how to test:
+1. run the app with: `python3 app.py`
+2. go to the given test link through my terminal in my browser
+3. try searching for:
+   - “banana” (should return banana oatmeal and banana waffles, etc)
+   - “vegan” (should return only vegan recipes)
+   - “spicy” (should return recipes with spicy flavor tags)
+
+test data:
+make sure my `recipes.csv` includes valid headers and rows matching the required format. See README for details.
+
+"""
+
